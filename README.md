244a2acaef384e2882553f7e8b965d1703cde9ea
Eisi Sig <eisi@meniga.is>
2015-11-13 11:33:26 +0000

    Added the ability to parse description as markdown if enabled with query

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ var docs = require("!!docgen!./Component.jsx");
 // => returns Component.jsx documentation object if Component definition is found
 ```
 
+### Markdown description
+
+If the description is in markdown you can parse it with `marked` by adding a query
+
+``` javascript
+var docs = require("!!docgen?markdownDescription!./Component.jsx");
+```
+
 > NOTE: I use double !! to disable all other loaders. react-docgen cannot gather Component information if the Component has been babeled
 
 ## License
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Eisi Sig @eisisig
-*/
+ MIT License http://www.opensource.org/licenses/mit-license.php
+ Author Eisi Sig @eisisig
+ */
 var docgen = require('react-docgen');
+var marked = require('marked');
+var loaderUtils = require('loader-utils');
 
-module.exports = function(source) {
+module.exports = function ( source ) {
 	this.cacheable && this.cacheable();
+	var query = loaderUtils.parseQuery(this.query);
 
 	var value = {};
 
 	try {
 		value = docgen.parse(source);
+		if ( query.markdownDescription && value.description ) {
+			value.description = marked(value.description);
+		}
 	} catch ( e ) {
 		// docgen - Error: No suitable component definition found.
 	}
 
 	this.values = [value];
 	return "module.exports = " + JSON.stringify(value, undefined, "\t");
-}
+};
